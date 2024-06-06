311eb148eaae394cb310bd11e62ad18dea849341
karelmon <karelgomez124@gmail.com>
2024-06-06 12:28:57 -0400

    start

---
+++ backend-audiencia/database/migrations/2024_06_06_162057_create__hogares_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Hogares', function (Blueprint $table) {
+            $table->id();
+            $table-> string('nombre',255);
+            $table-> string('nivel_socioeconomico',255);
+            $table-> string('proveedor_internet',255);
+            $table-> decimal('latidud',19,15);
+            $table-> decimal('longitud',19,15);
+            $table-> boolean('raspberry_asignada');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Hogares');
+    }
+};
